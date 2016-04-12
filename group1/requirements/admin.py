88b637ef5c7ca17ae1b9b835634fe06414381ba1
atshudy <allen.tshudy@gmail.com>
2016-04-11 20:12:03 -0400

    Removed some references to import the model baclog.py which doesn't exists.

--- group1/requirements/admin.py
+++ group1/requirements/admin.py
@@ -8,7 +8,6 @@ from requirements.models import story_comment
 from requirements.models import story_attachment
 from requirements.models import task
 from requirements.models import user_association
-from requirements.models import backlog
 
 admin.site.register(files.ProjectFile)
 admin.site.register(iteration.Iteration)
@@ -17,4 +16,3 @@ admin.site.register(story.Story)
 admin.site.register(story_comment.StoryComment)
 admin.site.register(story_attachment.StoryAttachment)
 admin.site.register(user_association.UserAssociation)
-admin.site.register(backlog.Backlog)
