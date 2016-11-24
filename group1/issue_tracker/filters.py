c4fe0a71b8ada980c8cbe1e5a376fb622f643c5d
antvalencia <anthony.valencia@intelliatx.com>
2016-11-24 16:27:43 +0000

    reformatting

--- group1/issue_tracker/filters.py
+++ group1/issue_tracker/filters.py
@@ -1,4 +1,3 @@
-
 import datetime
 
 from issue_tracker import models
@@ -48,34 +47,30 @@ class FilterIssueQueryset(object):
         return query
 
     def convert_date_min(self, item):
-        return timezone.make_aware(datetime.datetime.combine(
-            item, datetime.time.min),
-            timezone.get_current_timezone())
+        return timezone.make_aware(datetime.datetime.combine(item, datetime.time.min), timezone.get_current_timezone())
 
     def convert_date_max(self, item):
-        return timezone.make_aware(datetime.datetime.combine(
-            item, datetime.time.max),
-            timezone.get_current_timezone())
+        return timezone.make_aware(datetime.datetime.combine(item, datetime.time.max), timezone.get_current_timezone())
 
     def filter_submitted_date(self, query):
         if self.submitted_date:
-            query &= Q(submitted_date__range=(
-                self.convert_date_min(self.submitted_date),
-                self.convert_date_max(self.submitted_date)))
+            query &= Q(submitted_date__range=(self.convert_date_min(self.submitted_date),
+                                              self.convert_date_max(self.submitted_date))
+                       )
         return query
 
     def filter_modified_date(self, query):
         if self.modified_date:
-            query &= Q(modified_date__range=(
-                self.convert_date_min(self.modified_date),
-                self.convert_date_max(self.modified_date)))
+            query &= Q(modified_date__range=(self.convert_date_min(self.modified_date),
+                                             self.convert_date_max(self.modified_date))
+                       )
         return query
 
     def filter_closed_date(self, query):
         if self.closed_date:
-            query &= Q(closed_date__range=(
-                self.convert_date_min(self.closed_date),
-                self.convert_date_max(self.closed_date)))
+            query &= Q(closed_date__range=(self.convert_date_min(self.closed_date),
+                                           self.convert_date_max(self.closed_date))
+                       )
         return query
 
     def filter_assignee(self, query):
@@ -102,12 +97,12 @@ def filter_issue_results(data):
     Returns:
       A list of issues found.
     """
+
     query = Q()
     final_query = []
     filter_queryset = FilterIssueQueryset(data)
 
-    # Based on the parameters from the form, find the appropriate filter
-    # method and call it against the current queryset.
+    # Based on parameters from the form, find the appropriate filter method and call it against the current queryset.
     for key in data.iterkeys():
         handler = getattr(filter_queryset, 'filter_%s' % key)
         if handler:
