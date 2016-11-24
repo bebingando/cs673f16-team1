527d5c2803d3419c1fbb3d76a3113e25bcf52aa8
antvalencia <anthony.valencia@intelliatx.com>
2016-11-24 16:34:32 +0000

    refactoring

--- group1/issue_tracker/utils.py
+++ group1/issue_tracker/utils.py
@@ -9,35 +9,36 @@ from issue_tracker import models as it_models
 from requirements.models import project_api
 from requirements.models import user_association as user_association_model
 
-USERS = (('Mike', 'Bibriglia', 'mbibrigl',),
-         ('Bill', 'Cosby', 'bcosby',),
-         ('John', 'Stewart', 'jstewart',),
-         ('John', 'Oliver', 'joliver',),
-         ('Stephen', 'Colbert', 'scolbert',),
-         ('Conan', 'Obrien', 'teamcoco',),
-         ('Tina', 'Fey', 'thirtyrockfey',),
-         ('George', 'Carlin', 'gcarlin',),
-         ('Jonny', 'Carson', 'jcarson',),
-         ('George', 'Burns', 'jburns',),
-         ('Whitney', 'Cummings', 'whitcummings',),
-         ('Daniel', 'Tosh', 'tosho',),
-         ('Kevin', 'Hart', 'khart',),
-         ('Lucille', 'Ball', 'lball',),
-         ('Rosanne', 'Barr', 'rbarr',),
-         ('Billy', 'Crystal', 'bcrystal',),
-         ('Robin', 'Williams', 'aladin',),
-         ('Jim', 'Carrey', 'mask4ever',),
-         ('Dave', 'Chappelle', 'threeseason',),
-         ('Ellen', 'DeGeneres', 'ellen',),
-         ('Jeff', 'Foxworthy', 'mightberedneck',),
-         ('Zach', 'Galifanakis', 'twoferns',),
-         ('Whoopi', 'Goldberg', 'whoopi',),
-         ('Jimmy', 'Fallon', 'fallon',),
-         ('Andy', 'Kaufman', 'manonmoon',),
-         ('Jay', 'Leno', 'leno',),
-         ('Denis', 'Leary', 'dleary',),
-         ('Steve', 'Martin', 'crazywildguy',),
-         )
+USERS = (
+    ('Mike', 'Bibriglia', 'mbibrigl',),
+    ('Bill', 'Cosby', 'bcosby',),
+    ('John', 'Stewart', 'jstewart',),
+    ('John', 'Oliver', 'joliver',),
+    ('Stephen', 'Colbert', 'scolbert',),
+    ('Conan', 'Obrien', 'teamcoco',),
+    ('Tina', 'Fey', 'thirtyrockfey',),
+    ('George', 'Carlin', 'gcarlin',),
+    ('Jonny', 'Carson', 'jcarson',),
+    ('George', 'Burns', 'jburns',),
+    ('Whitney', 'Cummings', 'whitcummings',),
+    ('Daniel', 'Tosh', 'tosho',),
+    ('Kevin', 'Hart', 'khart',),
+    ('Lucille', 'Ball', 'lball',),
+    ('Rosanne', 'Barr', 'rbarr',),
+    ('Billy', 'Crystal', 'bcrystal',),
+    ('Robin', 'Williams', 'aladin',),
+    ('Jim', 'Carrey', 'mask4ever',),
+    ('Dave', 'Chappelle', 'threeseason',),
+    ('Ellen', 'DeGeneres', 'ellen',),
+    ('Jeff', 'Foxworthy', 'mightberedneck',),
+    ('Zach', 'Galifanakis', 'twoferns',),
+    ('Whoopi', 'Goldberg', 'whoopi',),
+    ('Jimmy', 'Fallon', 'fallon',),
+    ('Andy', 'Kaufman', 'manonmoon',),
+    ('Jay', 'Leno', 'leno',),
+    ('Denis', 'Leary', 'dleary',),
+    ('Steve', 'Martin', 'crazywildguy',),
+)
 
 DESCRIPTIONS = (
     "Everybody let's go. Go. \nJump up, wiggle and giggle with the "
@@ -50,7 +51,8 @@ DESCRIPTIONS = (
     "Touch them just like me.\n\n"
     "Pat your knees.\nPat your knees.\nPat them just like me.\n\n"
     "Stomp your feet.\nStomp your feet.\nStomp them just like me.\n\n"
-    "Clap your hands.\nClap your hands.\nNow let them quiet be.\n",)
+    "Clap your hands.\nClap your hands.\nNow let them quiet be.\n",
+)
 
 TITLES = (
     "I stole a pot.",
@@ -67,7 +69,7 @@ TITLES = (
     "General. Failing. Help",
     "Another random bug.",
     "I want my bikeshed painted BLUE though!",
-    )
+)
 
 COMMENTS = (
     "I just wanted to say all of this stuff is a buncha hooey.",
@@ -78,7 +80,7 @@ COMMENTS = (
     " cold rational brain.",
     "This is an automated statement: 42.",
     "I forgot to say in the description.. What a wonderful world.",
-    )
+)
 
 PROJECTS = (
     ("Issue tracking project",
@@ -92,7 +94,7 @@ PROJECTS = (
      "Sorta like the book series. You can learn a lot from a dummy!"),
     ("Big Ole Project",
      "It is about this big. *makes hand gesture*"),
-    )
+)
 
 
 def create_user(first, last, username, out_handle=None):
@@ -104,17 +106,17 @@ def create_user(first, last, username, out_handle=None):
       username: User handle.
       out_handle: The output handler for printing.
     """
+
     try:
-        user = User.objects.create_user(username=username,
-                                        email='%s@bu.edu' % username,
-                                        password='%spw' % username,
-                                        **{'first_name': first,
-                                           'last_name': last})
+        user = User.objects.create_user(
+            username=username,
+            email='%s@bu.edu' % username,
+            password='%spw' % username, **{'first_name': first, 'last_name': last}
+        )
         user.save()
     except IntegrityError:
         if out_handle:
-            out_handle.write(
-                'User already exists, skipping creation: %s' % username)
+            out_handle.write('User already exists, skipping creation: %s' % username)
 
 
 def create_users(users, out_handle=None):
@@ -124,6 +126,7 @@ def create_users(users, out_handle=None):
       users: A tuple of tuples containing first name, last name and user.
       out_handle: The output handler for printing.
     """
+
     if out_handle:
         out_handle.write('\nCreating users...')
     for full_user in users:
@@ -140,24 +143,23 @@ def create_super_user(first, last, username, out_handle=None):
       username: User handle.
       out_handle: The output handler for printing.
     """
+
     if out_handle:
         out_handle.write('\nCreating Super User...')
     try:
-        user = User.objects.create_superuser(username,
-                                             email='%s@bu.edu' % username,
-                                             password='%spw' % username,
-                                             **{'first_name': first,
-                                                'last_name': last})
+        user = User.objects.create_superuser(
+            username,
+            email='%s@bu.edu' % username,
+            password='%spw' % username, **{'first_name': first, 'last_name': last}
+        )
         user.save()
     except IntegrityError:
         if out_handle:
-            out_handle.write('User already exists, skipping creation: %s'
-                             % username)
+            out_handle.write('User already exists, skipping creation: %s' % username)
 
 
 def get_random_date():
-    return datetime.datetime.now() + datetime.timedelta(
-        days=random.randint(-365, 365))
+    return datetime.datetime.now() + datetime.timedelta(days=random.randint(-365, 365))
 
 
 def create_issues(number_of_issues, out_handle=None):
@@ -166,9 +168,10 @@ def create_issues(number_of_issues, out_handle=None):
     The issues will start in various states with random assignees.
 
     Args:
-      num: The number of issues to be created.
+      number_of_issues: The number of issues to be created.
       out_handle: The output handler for printing.
     """
+
     project_ids = []
     for project in project_api.get_all_projects():
         project_ids.append(project.pk)
@@ -182,41 +185,32 @@ def create_issues(number_of_issues, out_handle=None):
             out_handle.flush()
         # Go through a few extra hoops to make sure that if we create a issue
         # with any status other than new, it has been assigned to someone.
-        status = it_models.STATUSES[
-            random.randint(0, len(it_models.STATUSES) - 1)][0]
-        p = project_api.get_project(
-            project_ids[random.randint(0, len(project_ids) - 1)])
+        status = it_models.STATUSES[random.randint(0, len(it_models.STATUSES) - 1)][0]
+        p = project_api.get_project(project_ids[random.randint(0, len(project_ids) - 1)])
         # Limit the users to the developers on the project
-        user_ids = [user_ass.user.pk
-                    for user_ass in project_api.get_project_users(p.id)]
+        user_ids = [user_ass.user.pk for user_ass in project_api.get_project_users(p.id)]
         if status != 'new':
-            assignee = User.objects.get(
-                pk=user_ids[random.randint(0, len(user_ids) - 1)])
+            assignee = User.objects.get(pk=user_ids[random.randint(0, len(user_ids) - 1)])
         else:
             assignee = None
         if random.randint(0, 1):
             verifier = None
         else:
-            verifier = User.objects.get(
-                pk=user_ids[random.randint(0, len(user_ids) - 1)])
+            verifier = User.objects.get(pk=user_ids[random.randint(0, len(user_ids) - 1)])
 
         issue = it_models.Issue.objects.create(
             title=TITLES[random.randint(0, title_count)],
-            description=DESCRIPTIONS[
-                random.randint(0, description_count)],
-            issue_type=it_models.TYPES[
-                random.randint(0, len(it_models.TYPES) - 1)][0],
+            description=DESCRIPTIONS[random.randint(0, description_count)],
+            issue_type=it_models.TYPES[random.randint(0, len(it_models.TYPES) - 1)][0],
             status=status,
-            priority=it_models.PRIORITIES[
-                random.randint(0, len(it_models.PRIORITIES) - 1)][0],
+            priority=it_models.PRIORITIES[random.randint(0, len(it_models.PRIORITIES) - 1)][0],
             project=p,
             modified_date=get_random_date(),
             submitted_date=get_random_date(),
-            reporter=User.objects.get(
-                pk=user_ids[random.randint(0, len(user_ids) - 1)]),
+            reporter=User.objects.get(pk=user_ids[random.randint(0, len(user_ids) - 1)]),
             assignee=assignee,
             verifier=verifier,
-            )
+        )
         comment_count = len(COMMENTS) - 1
         for _ in xrange(1, random.randint(1, 10)):
             it_models.IssueComment.objects.create(
@@ -225,7 +219,8 @@ def create_issues(number_of_issues, out_handle=None):
                 date=get_random_date(),
                 poster=User.objects.get(
                     pk=user_ids[random.randint(0, len(user_ids) - 1)]),
-                is_comment=True)
+                is_comment=True
+            )
 
     if out_handle:
         out_handle.write('\n')
@@ -244,10 +239,10 @@ def create_projects(number_of_projects, out_handle=None):
       number_of_projects: The number of projects to be created.
       out_handle: The output handler for printing.
     """
-    # This extra work for user ids is necessary due to the fact that the
-    # database might have been wiped, but the origin pk for the user still
-    # has been claimed, so we must gather the current pk list to apply them
-    # to create issues.
+
+    # This extra work for user ids is necessary due to the fact that the  database might have been wiped, but the origin
+    # pk for the user still has been claimed, so we must gather the current pk list to apply them to create issues.
+
     user_ids = []
     for user in User.objects.all():
         user_ids.append(user.pk)
@@ -258,13 +253,12 @@ def create_projects(number_of_projects, out_handle=None):
         if out_handle:
             out_handle.write('.', ending='')
             out_handle.flush()
-        fields = {'title': '%s: %d' % (
-                      PROJECTS[random.randint(0, project_count)][0],
-                      i),
-                  'description': PROJECTS[random.randint(0, project_count)][1]}
+        fields = {
+            'title': '%s: %d' % (PROJECTS[random.randint(0, project_count)][0], i),
+            'description': PROJECTS[random.randint(0, project_count)][1]
+        }
         excluded_users = [_get_random_user(user_ids)]
-        p = project_api.create_project(user=excluded_users[0],
-                                       fields=fields)
+        p = project_api.create_project(user=excluded_users[0], fields=fields)
 
         # Now associate some number of developers to the project.
         for _ in xrange(random.randint(1, 5)):
@@ -272,8 +266,7 @@ def create_projects(number_of_projects, out_handle=None):
             ids = set(user_ids) - set([user.pk for user in excluded_users])
             u = _get_random_user(list(ids))
             excluded_users.append(u)
-            project_api.add_user_to_project(
-                p.id, u.username, user_association_model.ROLE_DEVELOPER)
+            project_api.add_user_to_project(p.id, u.username, user_association_model.ROLE_DEVELOPER)
 
     if out_handle:
         out_handle.write('\n')
@@ -281,5 +274,6 @@ def create_projects(number_of_projects, out_handle=None):
 
 def wipe_db():
     """This will wipe the database of all Users and Issues."""
+
     User.objects.all().delete()
     it_models.Issue.objects.all().delete()
