de916cbf169b6ede09d2b71342362a86f1db2e78
antvalencia <anthony.valencia@intelliatx.com>
2016-11-24 16:36:35 +0000

    refactoring

--- group1/requirements/views/projects.py
+++ group1/requirements/views/projects.py
@@ -31,6 +31,7 @@ from requirements.models import filemaker
 
 PERMISSION_OWN_PROJECT = 'requirements.own_project'
 
+
 @login_required(login_url='/signin')
 def list_projects(request):
     # Loads the DashBoard template, which contains a list of the project the user is
@@ -42,7 +43,10 @@ def list_projects(request):
         'theUser': request.user,
         'associationsWithUser': project_api.get_associations_for_user(request.user.id)
     }
-    list=(str(project_api.get_projects_for_user(request.user.id))).replace("<Project: ","").replace(', ','').replace('[','').replace("]",'').split('>')
+    list = (str(project_api.get_projects_for_user(request.user.id))).replace("<Project: ", "").replace(', ',
+                                                                                                       '').replace('[',
+                                                                                                                   '').replace(
+        "]", '').split('>')
     # if request.user.is_authenticated():
     #     logedInUser = request.user
     #     logedInUser.set_unusable_password()
@@ -57,23 +61,22 @@ def project(request, projectID):
         return redirect('/req/projects')
 
     iterations = mdl_iteration.get_iterations_for_project(project)
-    association = UserAssociation.objects.get(
-        user=request.user,
-        project=project)
+    association = UserAssociation.objects.get(user=request.user, project=project)
     priorities = Story.PRIORITY_CHOICES
 
-    context = {'projects': project_api.get_projects_for_user(request.user.id),
-               'project': project,
-               'stories': mdl_story.get_stories_for_project(project),
-               'tasks': mdl_task.get_all_tasks(),
-               'comments': mdl_story_comment.get_all_comments(),
-               'priorities': priorities,
-               'attachments': mdl_story_attachment.get_all_attachments(),
-               'issues': mdl_issue.get_all_issues(),
-               'iterations': iterations,
-               'association': association,
-               'canOwnProject': request.user.has_perm(PERMISSION_OWN_PROJECT),
-               }
+    context = {
+        'projects': project_api.get_projects_for_user(request.user.id),
+        'project': project,
+        'stories': mdl_story.get_stories_for_project(project),
+        'tasks': mdl_task.get_all_tasks(),
+        'comments': mdl_story_comment.get_all_comments(),
+        'priorities': priorities,
+        'attachments': mdl_story_attachment.get_all_attachments(),
+        'issues': mdl_issue.get_all_issues(),
+        'iterations': iterations,
+        'association': association,
+        'canOwnProject': request.user.has_perm(PERMISSION_OWN_PROJECT),
+    }
     return render(request, 'ProjectDetail.html', context)
 
 
@@ -83,14 +86,14 @@ def new_project(request):
     if request.method == 'POST':
         form = ProjectForm(request.POST)
         name = str(request.POST['title'])
-        #list=(str(project_api.get_projects_for_user(request.user.id))).replace("<Project: ","").replace(', ','').replace('[','').replace("]",'').split('>')
-        #list = project_api.get_projects_for_user(request.user.id).values_list('title',flat=True)
-        #print list
+        # list=(str(project_api.get_projects_for_user(request.user.id))).replace("<Project: ","").replace(', ','').replace('[','').replace("]",'').split('>')
+        # list = project_api.get_projects_for_user(request.user.id).values_list('title',flat=True)
+        # print list
 
-        #if name in list:
-        if project_api.duplicate_project(request.user,request.POST):
+        # if name in list:
+        if project_api.duplicate_project(request.user, request.POST):
             form.if_dup(1)
-        if form.is_valid() :
+        if form.is_valid():
             project_api.create_project(request.user, request.POST)
             project = form.save(commit=False)
             # return redirect('/req/projects')
@@ -121,10 +124,12 @@ def edit_project(request, projectID):
     else:
         form = ProjectForm(instance=project)
 
-    context = {'projects': project_api.get_projects_for_user(request.user.id),
-               'canOwnProject': request.user.has_perm(PERMISSION_OWN_PROJECT),
-               'title': 'Edit Project',
-               'form': form, 'action': '/req/editproject/' + projectID, 'button_desc': 'Save Changes'}
+    context = {
+        'projects': project_api.get_projects_for_user(request.user.id),
+        'canOwnProject': request.user.has_perm(PERMISSION_OWN_PROJECT),
+        'title': 'Edit Project',
+        'form': form, 'action': '/req/editproject/' + projectID, 'button_desc': 'Save Changes'
+    }
     return render(request, 'ProjectSummary.html', context)
 
 
@@ -144,20 +149,23 @@ def delete_project(request, projectID):
     else:
         form = ProjectForm(instance=project)
 
-    context = {'projects': project_api.get_projects_for_user(request.user.id),
-               'canOwnProject': request.user.has_perm(PERMISSION_OWN_PROJECT),
-               'title': 'Delete Project',
-               'confirm_message': 'This is an unrevert procedure ! You will lose all information about this project !',
-               'form': form, 'action': '/req/deleteproject/' + projectID, 'button_desc': 'Delete Project'}
+    context = {
+        'projects': project_api.get_projects_for_user(request.user.id),
+        'canOwnProject': request.user.has_perm(PERMISSION_OWN_PROJECT),
+        'title': 'Delete Project',
+        'confirm_message': 'This is an unrevert procedure ! You will lose all information about this project !',
+        'form': form, 'action': '/req/deleteproject/' + projectID, 'button_desc': 'Delete Project'
+    }
     return render(request, 'ProjectSummary.html', context)
 
-#=========================================================================
+
+# =========================================================================
 # @login_required(login_url='/accounts/login/')
 # @permission_required('projects.own_project')
 # def createProject(request):
 #     proj = models.createProject(request.user, request.POST)
 #     return redirect('/projects')
-#=========================================================================
+# =========================================================================
 
 
 @login_required(login_url='/signin')
@@ -165,19 +173,11 @@ def list_users_in_project(request, projectID):
     project = project_api.get_project(projectID)
     if project is None:
         return redirect('/req/projects')
-    association = UserAssociation.objects.get(
-        user=request.user,
-        project=project)
+    association = UserAssociation.objects.get(user=request.user, project=project)
     users = project.users.all()
-    pmusers = User.objects.filter(
-        project__id=project.id,
-        userassociation__role=user_association.ROLE_OWNER)
-    devusers = User.objects.filter(
-        project__id=project.id,
-        userassociation__role=user_association.ROLE_DEVELOPER)
-    cliusers = User.objects.filter(
-        project__id=project.id,
-        userassociation__role=user_association.ROLE_CLIENT)
+    pmusers = User.objects.filter(project__id=project.id, userassociation__role=user_association.ROLE_OWNER)
+    devusers = User.objects.filter(project__id=project.id, userassociation__role=user_association.ROLE_DEVELOPER)
+    cliusers = User.objects.filter(project__id=project.id, userassociation__role=user_association.ROLE_CLIENT)
 
     context = {
         'project': project,
@@ -245,9 +245,7 @@ def manage_user_association(request, projectID, userID):
     form = SelectAccessLevelForm()
     the_project = project_api.get_project(projectID)
     the_user = User.objects.get(id=userID)
-    association = UserAssociation.objects.get(
-        user=the_user,
-        project=the_project)
+    association = UserAssociation.objects.get(user=the_user, project=the_project)
     role = association.role
 
     context = {
@@ -290,31 +288,27 @@ def get_attachments(request, projectID):
 
 @user_can_access_project()
 def upload_attachment(request, projectID):
-    
     if 'file' not in request.FILES:
         raise IOError("Missing file")
-    if request.FILES['file'].size > 10*1024*1024:
+    if request.FILES['file'].size > 10 * 1024 * 1024:
         raise IOError("File too large")
 
     form = FileForm(request.POST, request.FILES)
-    
-    if(form.is_valid()):
-        
-        #set up parameters for database insert
+
+    if (form.is_valid()):
+        # set up parameters for database insert
         fileObj = request.FILES['file']
         projID = project_api.get_project(projectID)
         fname = fileObj.name
         fileUUID = str(uuid.uuid4())
-        
-        #rename file object to have UUID as name to avoid conflicts when retrieving files
-        fileObj.name=fileUUID
-        
-        f = ProjectFile(file=fileObj,
-                        project=projID,
-                        name=fname,
-                        uuid=fileUUID)
+
+        # rename file object to have UUID as name to avoid conflicts when retrieving files
+        fileObj.name = fileUUID
+
+        f = ProjectFile(file=fileObj, project=projID, name=fname, uuid=fileUUID)
         f.save()
     return redirect(request.POST['referer'])
+
 
 @login_required(login_url='/signin')
 def issues(request, projectID):
@@ -325,48 +319,45 @@ def issues(request, projectID):
         return redirect('/req/projects')
 
     iterations = mdl_iteration.get_iterations_for_project(project)
-    association = UserAssociation.objects.get(
-        user=request.user,
-        project=project)
+    association = UserAssociation.objects.get(user=request.user, project=project)
 
-    context = {'projects': project_api.get_projects_for_user(request.user.id),
-               'project': project,
-               'stories': mdl_story.get_stories_for_project(project),
-               'tasks': mdl_task.get_all_tasks(),
-               'comments': mdl_story_comment.get_all_comments(),
-               'attachments': mdl_story_attachment.get_all_attachments(),
-               'issues': mdl_issue.get_all_issues(),
-               'iterations': iterations,
-               'association': association,
-               'canOwnProject': request.user.has_perm(PERMISSION_OWN_PROJECT),
+    context = {
+        'projects': project_api.get_projects_for_user(request.user.id),
+        'project': project,
+        'stories': mdl_story.get_stories_for_project(project),
+        'tasks': mdl_task.get_all_tasks(),
+        'comments': mdl_story_comment.get_all_comments(),
+        'attachments': mdl_story_attachment.get_all_attachments(),
+        'issues': mdl_issue.get_all_issues(),
+        'iterations': iterations,
+        'association': association,
+        'canOwnProject': request.user.has_perm(PERMISSION_OWN_PROJECT),
     }
     return render(request, 'IssueList.html', context)
-    
+
+
 @user_can_access_project()
 def delete_attachment(request, projectID):
-
-    uuid=request.GET.get(
-            'file',
-            '')
+    uuid = request.GET.get('file', '')
     mdl_attachment.delete(uuid)
-
     return redirect(request.META['HTTP_REFERER'])
+
 
 @user_can_access_project()
 def download_attachment(request, projectID):
-
     file = ProjectFile.objects.get(
         project=projectID,
         uuid=request.GET.get(
             'file',
-            ''))
+            '')
+    )
     response = HttpResponse(file.file)
     response['Content-Disposition'] = 'attachment; filename=' + file.name
     return response
 
 
 @user_can_access_project()
-def makefile(request,projectID):
+def makefile(request, projectID):
     response = HttpResponse()
     response['Content-Disposition'] = 'attachment; filename=my.txt'
     statement = filemaker.make_Statement(projectID)
@@ -376,20 +367,20 @@ def makefile(request,projectID):
 
 
 @user_can_access_project()
-def makepdf(request,projectID):
+def makepdf(request, projectID):
     from django.shortcuts import render, redirect
     from reportlab.platypus import SimpleDocTemplate
     from forms import TaskFormSet
     from forms import PDFForm
     # print("flag1")
 
-    args={}
+    args = {}
 
-    if request.method =='POST':
+    if request.method == 'POST':
         # print("flag2")
         form = PDFForm(request.POST)
 
-        args['iteration_description']= 'iteration_description' in request.POST
+        args['iteration_description'] = 'iteration_description' in request.POST
         args['iteration_duration'] = 'iteration_duration' in request.POST
         args['story_description'] = 'story_description' in request.POST
         args['story_reason'] = 'story_reason' in request.POST
@@ -401,21 +392,22 @@ def makepdf(request,projectID):
         args['story_points'] = 'story_points' in request.POST
         args['pie_chart'] = 'pie_chart' in request.POST
 
-        #print(args)
+        # print(args)
 
         response = HttpResponse(content_type='application/pdf')
         response['Content-Disposition'] = 'attachment; filename=pdf_report.pdf'
         tempcanvas = SimpleDocTemplate(response)
 
-        filemaker.process_pdf(tempcanvas,projectID, args)
+        filemaker.process_pdf(tempcanvas, projectID, args)
         return response
     else:
         # print("flag3")
         form = PDFForm()
-    context = {'title': 'Generate and Download Report',
-                   'form': form,
-                   'action': '/req/makepdf/' + projectID,
-                   'button_desc': 'Download'}
-
+    context = {
+        'title': 'Generate and Download Report',
+        'form': form,
+        'action': '/req/makepdf/' + projectID,
+        'button_desc': 'Download'
+    }
 
     return render(request, 'PDFDialog.html', context)
